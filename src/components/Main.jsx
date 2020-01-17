9015ca3a5e84638b5f75319c7635646e8415b267
Lior Kaufman <liorka1990@gmail.com>
2020-01-17 15:33:45 -0800

    added styling and part of game logic

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 // Components
 import Grid from "./Grid";
@@ -17,8 +17,99 @@ const Main =() => {
     const selectBox = (row, col) => {
         let gridCopy = JSON.parse(JSON.stringify(gridFull));
         gridCopy[row][col] = !gridCopy[row][col];
+        console.log(row, col)
         setGridFull(gridCopy)
     };
+
+    const seedBoxes = () => {
+        let gridCopy = JSON.parse(JSON.stringify(gridFull));
+        for (let rowIndex = 0; rowIndex < rows ; rowIndex++)
+        {
+            for (let colsIndex = 0; colsIndex < cols ; colsIndex++)
+            {
+                if (Math.floor(Math.random() * 4) === 1)
+                {
+                    gridCopy[rowIndex][colsIndex] = true
+                }
+            }
+        }
+        setGridFull(gridCopy)
+    };
+
+
+    const checkBoard = () => {
+        let gridCopy = JSON.parse(JSON.stringify(gridFull));
+        for (let rowIndex = 0; rowIndex < rows ; rowIndex++)
+        {
+            for (let colsIndex = 0; colsIndex < cols ; colsIndex++)
+            {
+                let neighbourCounter = 0;
+
+                // Not by the edges:
+                if ( rowIndex > 0 && rowIndex < rows && colsIndex > 0 && colsIndex < cols)
+                {
+
+                    // Top, right, down, left
+                    neighbourCounter = gridFull[rowIndex + 1][colsIndex]? neighbourCounter++: neighbourCounter;
+                    neighbourCounter = gridFull[rowIndex][colsIndex + 1]? neighbourCounter++: neighbourCounter;
+                    neighbourCounter = gridFull[rowIndex - 1][colsIndex]? neighbourCounter++: neighbourCounter;
+                    neighbourCounter = gridFull[rowIndex][colsIndex - 1]? neighbourCounter++: neighbourCounter;
+                    // Top left, top right, bottom right, bottom left
+                    neighbourCounter = gridFull[rowIndex + 1][colsIndex - 1]? neighbourCounter++: neighbourCounter;
+                    neighbourCounter = gridFull[rowIndex + 1][colsIndex + 1]? neighbourCounter++: neighbourCounter;
+                    neighbourCounter = gridFull[rowIndex - 1][colsIndex + 1]? neighbourCounter++: neighbourCounter;
+                    neighbourCounter = gridFull[rowIndex - 1][colsIndex - 1]? neighbourCounter++: neighbourCounter;
+
+                }
+
+                // Top row
+
+
+
+                switch (neighbourCounter) {
+                    case 1:
+                        gridCopy[rowIndex][colsIndex] = false;
+                        break;
+
+                    case 3:
+                        if (!gridFull[rowIndex][colsIndex]){
+                            gridCopy[rowIndex][colsIndex] = true;
+                        }
+                        break;
+
+                    case neighbourCounter > 3:
+                        if (gridFull[rowIndex][colsIndex])
+                        {
+                            gridCopy[rowIndex][colsIndex] = false;
+                        }
+
+                    default:
+                        gridCopy[rowIndex][colsIndex] = false;
+                        break;
+                }
+
+            }
+        }
+
+        setGridFull(gridCopy)
+
+    };
+
+    const playButton = () => {
+        setInterval()
+    }
+
+    const play = () => {
+        let gridCopy1 = gridFull
+        let gridCopy2 = JSON.parse(JSON.stringify(gridFull))
+        
+    }
+
+
+
+    useEffect(() => {
+        seedBoxes()
+    }, [])
 
     return (
         <div>
